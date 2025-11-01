4bebdd7a8dad1994e67d196d8473bddcdead0410
Felipe Borges <lipelsborges@gmail.com>
2025-11-01 11:28:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 //Carregando módulos
 const express = require('express')
-const handlebars = require('express-handlebars')
+const { engine } = require('express-handlebars');
 const bodyParser = require('body-parser')
 const app = express()
 //const mongoose = require('mongoose')
 
 // Configurações
+    //Body-Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    //Handlebars
+        app.engine('handlebars', engine({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    //Mongoose
 
 // Rotas
 
