bc960e778f630a44f6d5a7d1d724e5e89460bc3b
Felipe Borges <lipelsborges@gmail.com>
2025-11-01 17:34:44 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,13 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias")
+        Categoria.find().lean().then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias})
+    }).catch((error) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+    })
+    
 })
 
 router.get('/categorias/add', (req, res) => {
