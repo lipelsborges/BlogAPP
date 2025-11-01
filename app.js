78a4f8933c13923e39b7bbb564a8bb33ad7ac0ba
Felipe Borges <lipelsborges@gmail.com>
2025-11-01 14:32:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
 const mongoose = require('mongoose')
+const session = require("express-session")
+const flash = require("connect-flash")
 
 // Configurações
+    //Sessão
+        app.use(session({
+            secret: "blogapp_secreta",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) =>{
+            res.locals.success_msg = req.flash("success_msg")                      //Uma variavel global !
+            res.locals.error_msg = req.flash("error_msg")                          // Uma variavel global !
+            next()
+        })
     //Body-Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -23,6 +38,7 @@ const mongoose = require('mongoose')
         })
     //Public
         app.use(express.static(path.join(__dirname, "public")))
+
 //Rotas
     app.use('/admin', admin)
 // Outros
