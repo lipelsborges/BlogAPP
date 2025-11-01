58cd6beedbd3f01186702fbaba873cb3c823462c
Felipe Borges <lipelsborges@gmail.com>
2025-11-01 17:36:32 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-        Categoria.find().lean().then((categorias) => {
+        Categoria.find().lean().sort({date: 'desc'}).then((categorias) => {
         res.render("admin/categorias", {categorias: categorias})
     }).catch((error) => {
         req.flash("error_msg", "Houve um erro ao listar as categorias")
