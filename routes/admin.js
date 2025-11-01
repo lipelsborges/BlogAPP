33f65d303c362b5ead79c53d29c07edb7d7110b6
Felipe Borges <lipelsborges@gmail.com>
2025-11-01 16:38:25 -0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 require('../models/Categoria')
 const Categoria = mongoose.model("categorias")
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render("admin/index")
 })
 router.get('/posts', (req, res) => {
@@ -20,17 +20,41 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    let erros = []
+
+    if (!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+        erros.push({ texto: "Nome inválido" })
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria salva com sucesso!")
-    }).catch((error) => {
-        console.log("Erro ao salvar a categoria!")
-    })
+    if (!req.body.slug || typeof req.body.slug === undefined || req.body.nome === null) {
+        erros.push({ texto: "Slug inválido" })
+    }
 
-})
+    if (req.body.nome.length < 2) {
+        erros.push({ texto: "Nome da categoria muito pequeno" })
+
+    }
+
+    if (erros.length > 0) {
+        res.render("admin/addcategorias", { erros: erros })
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criado com sucesso! ")
+            res.redirect("/admin/categorias")
+        }).catch((error) => {
+            req.flash("error_msg","Houve um erro ao salvar a categoria, tente novamente!")
+            res.redirect("/admin")
+        })
+
+    }
+});
+
+
 
 module.exports = router
